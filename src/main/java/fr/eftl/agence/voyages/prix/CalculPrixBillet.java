26d7d1d419c22c4ab6adcc85fc0ecd9671e0a54c
MARGUERITE <chmarguerite@testlogiciel.pro>
2019-05-07 14:44:01 +0200

    test

--- src/main/java/fr/eftl/agence/voyages/prix/CalculPrixBillet.java
+++ src/main/java/fr/eftl/agence/voyages/prix/CalculPrixBillet.java
@@ -4,7 +4,6 @@ public class CalculPrixBillet {
 	
 	private CalculPrixUnitaire calculPrixUnitaire;
 	private Integer prixUnitaire;
-	private String classe;
 	
 	public Integer getPrixUnitaire() {
 		return prixUnitaire;
@@ -12,33 +11,29 @@ public class CalculPrixBillet {
 	public void setPrixUnitaire(Integer prixUnitaire) {
 		this.prixUnitaire = prixUnitaire;
 	}
-	public String getClasse() {
-		return classe;
-	}
-	public void setClasse(String classe) {
-		this.classe = classe;
-	}
-	
+
+
 	public Integer calculer(String depart,String arrivee, String classe) {
 		calculPrixUnitaire.setDepart(depart);
 		calculPrixUnitaire.setArrivee(arrivee);	
-		Integer varPrixUnitaire = calculPrixUnitaire.generer();
+		this.setPrixUnitaire(calculPrixUnitaire.generer());
+		Integer varPrixUnitaire = this.getPrixUnitaire();
+		Integer varPrixBillet=0;
 		switch(classe) {
 		case "eco":
-			return varPrixUnitaire;
+			varPrixBillet= varPrixUnitaire;
+			break;
 		case "premiere":
-			return varPrixUnitaire * 2;
+			varPrixBillet=  varPrixUnitaire * 2;
+			break;
 		case "business":
-			return varPrixUnitaire * 2 + 300;
-		default:
-			return varPrixUnitaire;
+			varPrixBillet=  varPrixUnitaire * 2 + 300;
+			break;
 		}
-		
+		return varPrixBillet;
 		
 	}
-	public CalculPrixUnitaire getCalculPrixUnitaire() {
-		return calculPrixUnitaire;
-	}
+
 	public void setCalculPrixUnitaire(CalculPrixUnitaire calculPrixUnitaire) {
 		this.calculPrixUnitaire = calculPrixUnitaire;
 	}
